a618e1d06d4652329c7deefad80877b0977f227c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-18 21:42:14 +0200

    setup skeleton for calculator

---
+++ src/components/Display.tsx
@@ -0,0 +1,7 @@
+export default function display() {
+  return (
+    <>
+      <div id="display">Display...</div>
+    </>
+  )
+}
---
+++ src/components/Operations.tsx
@@ -0,0 +1,12 @@
+export default function Operations() {
+  return (
+    <>
+      <button id="clear">AC</button>
+      <button id="equals">=</button>
+      <button id="add">+</button>
+      <button id="subtract">-</button>
+      <button id="multiply">*</button>
+      <button id="divide">/</button>
+    </>
+  )
+}
