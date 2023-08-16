c6f2133b3a52bdf17759a1bc479bff5fe4178056
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-08-16 21:22:19 +0200

    type props

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,9 +1,13 @@
-export default function display() {
+type Props = {
+  display: number
+}
+
+export default function display({ display }: Props) {
   return (
     <>
       <div id="display" className="output">
         <div className="current-operand">
-          Display...
+          { display }
         </div>
       </div>
     </>
